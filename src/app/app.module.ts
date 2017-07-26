b9b91d44a31d1a59243a17ce74393a196b4af010
Cristian Tavares de Freitas <cfreitas@crefitosp.local>
2017-07-26 15:23:50 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { FeedPage} from '../pages/feed/feed';
+import { IntroPage} from '../pages/intro/intro';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FeedPage,
+    IntroPage,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FeedPage,
+    IntroPage,
+    
+
   ],
   providers: [
     StatusBar,
---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  public nome_usuario: string = "Cristian Freitas";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  //public somaDoisNumeros(): void {
+  //  alert(5 + 10);
+  //}
+
+  public somaDoisNumeros(num1:number,num2:number): void {
+    alert(num1 + num2);
+  }
+
+  ionViewDidLoad() {
+   // this.somaDoisNumeros(10,99);
+  }
+
+}
